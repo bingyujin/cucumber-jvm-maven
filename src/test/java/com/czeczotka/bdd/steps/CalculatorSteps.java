8bddfac2da28fb9fc881f7ca69b67816591dd91c
Jane Jin(Bingyu) <jbyjane@qq.com>
2021-10-20 14:10:17 +0800

    aaa

--- src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
+++ src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
@@ -1,10 +1,13 @@
 package com.czeczotka.bdd.steps;
 
 import com.czeczotka.bdd.calculator.Calculator;
-import io.cucumber.java.Before;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import cucumber.api.PendingException;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 
 
 import static org.junit.Assert.assertEquals;
@@ -32,5 +35,53 @@ public class CalculatorSteps {
     @Then("^the result should be (\\d+)$")
     public void the_result_should_be(int result) throws Throwable {
         assertEquals(result, calculator.getResult());
+    }
+
+    @Given("^a global administrator named \"([^\"]*)\"$")
+    public void aGlobalAdministratorNamed(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^a blog named \"([^\"]*)\"$")
+    public void aBlogNamed(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^a customer named \"([^\"]*)\"$")
+    public void aCustomerNamed(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^the following people exist:$")
+    public void theFollowingPeopleExist() {
+        
+    }
+
+    @And("^some precondition (\\d+)$")
+    public void somePrecondition(int arg0) {
+        
+    }
+
+    @When("^some action by the actor$")
+    public void someActionByTheActor() {
+        
+    }
+
+    @And("^some other action$")
+    public void someOtherAction() {
+        
+    }
+
+    @Then("^some testable outcome is achieved$")
+    public void someTestableOutcomeIsAchieved() {
+        
+    }
+
+
+    @And("^something else we can check happens too$")
+    public void somethingElseWeCanCheckHappensToo() {
     }
 }
