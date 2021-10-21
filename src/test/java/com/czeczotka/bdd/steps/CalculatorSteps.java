696265f02d79c9f8c4b256486845e60b49f86822
Jane Jin(Bingyu) <jbyjane@qq.com>
2021-10-21 15:47:22 +0800

    update right step definition

--- src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
+++ src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
@@ -18,52 +18,9 @@ public class CalculatorSteps {
         //Assume.assumeTrue(false);
         calculator = new Calculator();
     }
-
-    @Given("^I have a calculator$")
-    public void i_have_a_calculator() throws Throwable {
-        //assertNotNull(calculator);
-    }
-
-    @When("^I add (\\d+) and (\\d+)$")
-    public void i_add_and(int arg1, int arg2) throws Throwable {
-        //calculator.add(arg1, arg2);
-    }
-
-    @Then("^the result should be (\\d+)$")
-    public void the_result_should_be(int result) throws Throwable {
-        //assertEquals(result, calculator.getResult());
-    }
-
+    //background steps
     @Given("a global administrator named {string}")
     public void aGlobalAdministratorNamed(String arg0) {
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Then("some testable outcome is achieved")
-    public void someTestableOutcomeIsAchieved() {
-        //throw new RuntimeException("fdsf");
-    }
-
-    @And("some other precondition with doc string")
-    public void someOtherPreconditionWithDocString(String a) {
-
-    }
-
-    @Given("some precondition")
-    public void somePrecondition() {
 
     }
     @Given("a blog named \"Greg's anti-tax rants\"")
@@ -75,39 +32,53 @@ public class CalculatorSteps {
 
     }
 
-
-    @When("some action by the actor")
-    public void someActionByTheActor() {
-
-    }
-
+    //Some determinable business situation steps
     @Given("the following people exist:")
     public void theFollowingPeopleExist(io.cucumber.datatable.DataTable table) {
     }
-
     @And("some precondition {int}")
     public void somePrecondition(int arg0) {
+
+    }
+    @When("some action by the actor")
+    public void someActionByTheActor() {
 
     }
 
     @And("some other action")
     public void someOtherAction() {
     }
+    @Then("some testable outcome is achieved")
+    public void someTestableOutcomeIsAchieved() {
+        //throw new RuntimeException("fdsf");
+    }
 
     @And("something else we can check happens too")
     public void somethingElseWeCanCheckHappensToo() {
     }
 
+
+
+    //Some another scenario 2 steps
+    @Given("some precondition")
+    public void somePrecondition() {
+
+    }
+    @And("some other precondition with doc string")
+    public void someOtherPreconditionWithDocString(String a) {
+
+    }
     @And("yet another action")
     public void yetAnotherAction() {
     }
-
     @But("I don't see something else")
     public void iDonTSeeSomethingElse() {
     }
 
-    @Given("the cow weighs <weight> kg")
-    public void theCowWeighsWeightKg() {
+
+    // outline steps
+    @Given("the cow weighs {int} kg")
+    public void theCowWeighsWeighctKg(int arg0) {
     }
 
     @When("we calculate the feeding requirements")
@@ -121,8 +92,25 @@ public class CalculatorSteps {
         }
         else{throw new RuntimeException("fdsf");}
     }
+    //no need
+//    @Given("^I have a calculator$")
+//    public void i_have_a_calculator() throws Throwable {
+//        //assertNotNull(calculator);
+//    }
+//
+//    @When("^I add (\\d+) and (\\d+)$")
+//    public void i_add_and(int arg1, int arg2) throws Throwable {
+//        //calculator.add(arg1, arg2);
+//    }
+//
+//    @Then("^the result should be (\\d+)$")
+//    public void the_result_should_be(int result) throws Throwable {
+//        //assertEquals(result, calculator.getResult());
+//    }
 
-    @Given("the cow weighs {int} kg")
-    public void theCowWeighsWeighctKg(int arg0) {
-    }
+//    @Given("the cow weighs <weight> kg")
+//    public void theCowWeighsWeightKg() {
+//    }
+
+
 }
