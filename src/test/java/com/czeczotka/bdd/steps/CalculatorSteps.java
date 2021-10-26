84a6b1c49bbfc2c6cc5f0f4f152a661bb8042d0a
Jane Jin(Bingyu) <jbyjane@qq.com>
2021-10-26 14:28:57 +0800

    a

--- src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
+++ src/test/java/com/czeczotka/bdd/steps/CalculatorSteps.java
@@ -1,13 +1,10 @@
 package com.czeczotka.bdd.steps;
 
 import com.czeczotka.bdd.calculator.Calculator;
-import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.*;
-
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import io.cucumber.datatable.DataTable;
+import org.junit.Assert;
 
 public class CalculatorSteps {
 
@@ -18,86 +15,126 @@ public class CalculatorSteps {
         calculator = new Calculator();
     }
 
-    @Given("^I have a calculator$")
-    public void i_have_a_calculator() throws Throwable {
-        assertNotNull(calculator);
-    }
-
-    @When("^I add (\\d+) and (\\d+)$")
-    public void i_add_and(int arg1, int arg2) throws Throwable {
-        calculator.add(arg1, arg2);
-    }
-
-    @Then("^the result should be (\\d+)$")
-    public void the_result_should_be(int result) throws Throwable {
-        assertEquals(result, calculator.getResult());
-    }
+        //background steps
 
     @Given("^a global administrator named \"([^\"]*)\"$")
     public void aGlobalAdministratorNamed(String arg0) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        //throw new PendingException();
+    }
+        @Given("a blog named \"Greg's anti-tax rants\"")
+        public void saomePrecondition() {
+
+        }
+        @Given("a customer named \"Wilson\"")
+        public void saomsePrecondition() {
+
+        }
+
+        //Some determinable business situation steps
+        @Given("the following people exist:")
+        public void theFollowingPeopleExist(DataTable table) {
+            //io.cucumber.datatable.DataTable table
+        }
+//        @And("some precondition {int}")
+//        public void somePrecondition(int arg0) {
+//
+//        }
+        @When("some action by the actor")
+        public void someActionByTheActor() {
+
+        }
+
+        @And("some other action")
+        public void someOtherAction() {
+        }
+        @Then("some testable outcome is achieved")
+        public void someTestableOutcomeIsAchieved() {
+            //throw new RuntimeException("fdsf");
+        }
+
+        @And("something else we can check happens too")
+        public void somethingElseWeCanCheckHappensToo() {
+        }
+
+
+
+        //Some another scenario 2 steps
+        @Given("some precondition (\\d+)")
+        public void somePrecondition(int arg0) {
+
+        }
+        @And("some other precondition with doc string")
+        public void someOtherPreconditionWithDocString(String a) {
+
+        }
+        @And("yet another action")
+        public void yetAnotherAction() {
+        }
+        @But("I don't see something else")
+        public void iDonTSeeSomethingElse() {
+        }
+
+
+        // outline steps
+        @Given("^the cow weighs (\\d+) kg$")
+        public void theCowWeighsWeightKg(int arg0) {
+        throw new RuntimeException();
+        //Assert.assertEquals(3,1);
+        //"(\d+)"
+        }
+
+        @When("we calculate the feeding requirements")
+        public void weCalculateTheFeedingRequirements() {
+        }
+
+    @Then("^the energy should be (\\d+) MJ$")
+    public void theEnergyShouldBeEnergyMJ(int arg0) {
     }
 
-    @And("^a blog named \"([^\"]*)\"$")
-    public void aBlogNamed(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @And("^a customer named \"([^\"]*)\"$")
-    public void aCustomerNamed(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
-    @Given("^the following people exist:$")
-    public void theFollowingPeopleExist() {
-        
-    }
-
-    @And("^some precondition (\\d+)$")
-    public void somePrecondition(int arg0) {
-        
-    }
-
-    @When("^some action by the actor$")
-    public void someActionByTheActor() {
-        
-    }
-
-    @And("^some other action$")
-    public void someOtherAction() {
-        
-    }
-
-    @Then("^some testable outcome is achieved$")
-    public void someTestableOutcomeIsAchieved() {
-        throw new PendingException();
-        
-    }
-
-
-    @And("^something else we can check happens too$")
-    public void somethingElseWeCanCheckHappensToo() {
-    }
-
-    @Given("^some precondition$")
+    @Given("some precondition")
     public void somePrecondition() {
-
     }
 
-    @And("^some other precondition with doc string$")
-    public void someOtherPreconditionWithDocString() {
 
-    }
+//        @Then("the energy should be {int} MJ")
+//        public void theEnergyShouldBeEnergyMJ(int arg0) {
+////        if(arg0==26500) {
+////            assertEquals(2, 3);
+////        }
+////        else{throw new RuntimeException("fdsf");}
+////        if(arg0==26500) {
+////            throw new RuntimeException();
+////        }
+////        else{
+////        }
+//            Assert.assertTrue(true);
+//        }
 
-    @And("^yet another action$")
-    public void yetAnotherAction() {
 
-    }
 
-    @But("^I don't see something else$")
-    public void iDonTSeeSomethingElse() {
-    }
+
+    //no need
+//    @Given("^I have a calculator$")
+//    public void i_have_a_calculator() throws Throwable {
+//        //assertNotNull(calculator);
+//    }
+//
+//    @When("^I add (\\d+) and (\\d+)$")
+//    public void i_add_and(int arg1, int arg2) throws Throwable {
+//        //calculator.add(arg1, arg2);
+//    }
+//
+//    @Then("^the result should be (\\d+)$")
+//    public void the_result_should_be(int result) throws Throwable {
+//        //assertEquals(result, calculator.getResult());
+//    }
+
+//    @Given("the cow weighs <weight> kg")
+//    public void theCowWeighsWeightKg() {
+//    }
+
+
+
+
 }
